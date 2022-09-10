267b07319769b6df205e8133755412189ff83705
Probmkr <Thanatos9911@gmail.com>
2022-09-11 01:01:40 +0900

    temp

--- src/cogs.py
+++ src/cogs.py
@@ -15,7 +15,23 @@ class Others(commands.Cog):
         # guild = await self.bot.fetch_guild(MAIN_GUILD)
         channel = await self.bot.fetch_channel(GM_CHANNEL)
         logger.log(LT.INFO, f"logged in as {self.bot.user}")
-        await channel.send("botが起動しました")
+        all_slash_commands = await self.bot.fetch_global_commands()
+        for i in all_slash_commands:
+            logger.log(LT.DEBUG, i.name)
+        # await channel.send("botが起動しました")
+
+class Werewolf(commands.Cog):
+    def __init__(self, bot: commands.Bot):
+        self.bot = bot
+
+    @commands.slash_command(description="人狼ゲームに関するメインコマンド")
+    async def wolf(self):
+        pass
+
+    @wolf.sub_command(description="人狼ゲームを開始します")
+    async def start(self, interaction: disnake.ApplicationCommandInteraction):
+        await interaction.response.send_message("dummy")
 
 def setup(bot: commands.Bot):
+    bot.add_cog(Werewolf(bot))
     bot.add_cog(Others(bot))
